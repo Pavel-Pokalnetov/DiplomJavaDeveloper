6deaa689683f0ce6458a489d34c58b6bca5f3d05
Pavel-Pokalnetov <redx@mail.ru>
2024-02-27 12:36:01 +0500

    ++

--- src/main/java/ru/slenergo/AppMonitoring/controllers/ReportController.java
+++ src/main/java/ru/slenergo/AppMonitoring/controllers/ReportController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.slenergo.AppMonitoring.model.DataSummary;
 import ru.slenergo.AppMonitoring.services.ReportService;
 
@@ -21,33 +22,42 @@ public class ReportController {
     ReportService reportService;
 
     @GetMapping("")
-    public String reportMainPage(){
+    public String reportMainPage() {
         return "report/mainReport";
     }
 
-    /** Сводный отчет за текущие сутки
+    /**
+     * Сводный отчет за текущие сутки
+     *
      * @param model
      * @return
      */
     @GetMapping("/summary")
-    public String summaryReport(Model model){
+    public String summaryReport(Model model) {
         List<DataSummary> dataSummaryList = reportService.getSummaryReportToDay();
         model.addAttribute(dataSummaryList);
-        model.addAttribute("currentdate",LocalDate.now().format(formatterDateOnly));
+        model.addAttribute("currentdate", LocalDate.now().format(formatterDateOnly));
         return "report/summaryReport";
     }
 
-    /** Обновление сводного отчета за текущие сутки
+    /**
+     * Обновление сводного отчета за текущие сутки
+     *
      * @return
      */
     @GetMapping("/summary/update")
-    public String summaryReportUpdate(){
+    public String summaryReportUpdate() {
         reportService.recalcSummaryReportByDate(LocalDateTime.now());
         return "redirect:/report/summary";
     }
 
     @GetMapping("/dayreports")
-    public String dayReports(Model model){
-        return "/report/dayReports";
+    public String dayReports(@RequestParam(required = false) LocalDate date, Model model) {
+        if (date == null) {
+            model.addAttribute("dateIsDefine", false);
+            model.addAttribute();
+            return "report/dayReports";
+        }
+        return "report/dayReports";
     }
 }
