3a26e48051b868aea28ac2ce494167528665e6b6
Pavel-Pokalnetov <redx@mail.ru>
2024-03-12 18:28:07 +0500

    fixing test

--- src/test/java/ru/slenergo/AppMonitoring/etc/StaticToolsTest.java
+++ src/test/java/ru/slenergo/AppMonitoring/etc/StaticToolsTest.java
@@ -1,22 +1,20 @@
-package ru.slenergo.AppMonitoring.security;
+package ru.slenergo.AppMonitoring.etc;
 
 
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.slenergo.AppMonitoring.etc.StaticTools.dropSmallDecimalPart;
 
-@SpringBootTest
+
 public class StaticToolsTest {
     @Test
-    public void dropSmallDecimalPartTest(){
-        assertEquals(dropSmallDecimalPart(12.12345,1),12.1,0.0);
-        assertEquals(dropSmallDecimalPart(-45.461,1),-45.4,0.0);
-        assertEquals(dropSmallDecimalPart(0.0001,4),0.0001,0.0);
-        assertEquals(dropSmallDecimalPart(1.9999,1),1.9,0.0);
+    public void dropSmallDecimalPartTest() {
+        Assertions.assertEquals(dropSmallDecimalPart(12.12345, 1), 12.1, 0.0);
+        Assertions.assertEquals(dropSmallDecimalPart(-45.461, 1), -45.4, 0.0);
+        Assertions.assertEquals(dropSmallDecimalPart(0.0001, 4), 0.0001, 0.0);
+        Assertions.assertEquals(dropSmallDecimalPart(1.9999, 1), 1.9, 0.0);
 
     }
 }
